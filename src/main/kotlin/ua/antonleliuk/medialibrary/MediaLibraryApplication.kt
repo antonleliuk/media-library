0afa0cf3124a7b5a8d9f2b1f5e29296c64a3d0c2
Anton Leliuk <anton.leluk@gmail.com>
2020-01-27 01:45:12 +0200

    Initial commit

---
+++ src/main/kotlin/ua/antonleliuk/medialibrary/MediaLibraryApplication.kt
@@ -0,0 +1,11 @@
+package ua.antonleliuk.medialibrary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MediaLibraryApplication
+
+fun main(args: Array<String>) {
+	runApplication<MediaLibraryApplication>(*args)
+}
---
+++ src/test/kotlin/ua/antonleliuk/medialibrary/MediaLibraryApplicationTests.kt
@@ -0,0 +1,13 @@
+package ua.antonleliuk.medialibrary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MediaLibraryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
